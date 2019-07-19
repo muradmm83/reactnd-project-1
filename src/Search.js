a2d3233806fa7e14440d462900180d0db93083e0
Murad <muradmm83@gmail.com>
2019-07-19 14:13:35 +0300

    feat: enable book search

--- src/Search.js
+++ src/Search.js
@@ -1,7 +1,56 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import * as BooksAPI from './BooksAPI';
+import Book from './Book';
 
 class Search extends Component {
+
+    state = {
+        searchResult: [],
+        typingTimeout: 0
+    }
+
+    query(term) {
+        if (this.state.typingTimeout) {
+            clearTimeout(this.state.typingTimeout);
+        }
+
+        this.setState(currentState => ({
+            ...currentState,
+            typingTimeout: setTimeout(() => {
+                BooksAPI.search(term)
+                    .then(books => {
+                        if (!books.error) {
+                            let foundBooks = books.map(b => ({
+                                id: b.id,
+                                title: b.title,
+                                authors: b.authors,
+                                imageUrl: b.imageLinks.thumbnail,
+                                shelf: 'none'
+                            }));
+
+                            foundBooks = foundBooks.map(fb => {
+                                const book = this.props.books.find(b => fb.id === b.id);
+
+                                return {
+                                    ...fb,
+                                    shelf: book ? book.shelf : fb.shelf
+                                }
+                            });
+
+                            this.setState(currentState => ({
+                                ...currentState,
+                                searchResult: foundBooks
+                            }));
+                        }
+                    });
+            }, 550)
+        }));
+
+
+    }
+
     render() {
         return (
             <div className="search-books">
@@ -10,15 +59,21 @@ class Search extends Component {
                         <button className="close-search">Close</button>
                     </Link>
                     <div className="search-books-input-wrapper">
-                        <input type="text" placeholder="Search by title or author" />
+                        <input type="text" placeholder="Search by title or author" onChange={e => this.query(e.target.value)} />
                     </div>
                 </div>
                 <div className="search-books-results">
-                    <ol className="books-grid"></ol>
+                    <ol className="books-grid">
+                        {this.state.searchResult.map(b => (<Book key={b.id} book={b} />))}
+                    </ol>
                 </div>
             </div>
         );
     }
+}
+
+Search.propTypes = {
+    books: PropTypes.array.isRequired
 }
 
 export default Search;
